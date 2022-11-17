27249da941ba307ea8a47e18b83881314c4e1139
cencen16 <117888095+cencen16@users.noreply.github.com>
2022-11-17 13:51:23 +0700

    Add files via upload

---
+++ jennifer UG_9 soal 2.py
@@ -0,0 +1,3 @@
+print (" =========== INI PROGRAM SEDERHANA ==========")
+a = int(input("Masukkan angka yang ingin di pangkatkan : >> ")
+print (" Angka anda telah di pangkat kan 7! anda mendapat angka : 823543 ")
