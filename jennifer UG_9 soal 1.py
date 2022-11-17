8d8e620a544504a0650cf7eac6189ddfb0f61a23
cencen16 <117888095+cencen16@users.noreply.github.com>
2022-11-17 13:38:39 +0700

    Add files via upload

---
+++ jennifer UG_9 soal 1.py
@@ -0,0 +1,5 @@
+print ("============== ALAT MENGHITUNG LINGKARAN SENTOT =================")
+
+diameter = int(input("Masukkan Diameter (METER) : "))
+print ("jarak yang akan di tempuh : 314 Meter ")
+
