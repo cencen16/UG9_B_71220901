e2e30eb2c1f00ac7473e08d7c1465a3a637cc892
cencen16 <117888095+cencen16@users.noreply.github.com>
2022-11-17 12:55:45 +0700

    Add files via upload

---
+++ UG 9 soal 1 grup B.py
@@ -0,0 +1,12 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: D:/JenniferUG_9/jenniferUG_9.py ===================
+=======ALAT PENGHITUNG LINGKARAN SENTOT=======
+Masukan Diameter (METER) : 100
+Jarak yang harus di tempuh : 27737387192
+>>> 
+===================== RESTART: D:/Python/UG 1 soal 2.py =====================
+=======INI PROGRAM SEDER HANA ========
+masukkan Angka yang ingin di pangkatkan : 7
+Angka anda telah di pangkat kan 7! anda mendapat angka: 823543
